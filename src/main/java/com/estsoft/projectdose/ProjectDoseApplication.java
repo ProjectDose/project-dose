afde0e4a26368213b96e050d6d576486f27bf67c
junsik2 <arc551234@gmail.com>
2024-11-28 23:48:44 +0900

    init project

---
+++ src/main/java/com/estsoft/projectdose/ProjectDoseApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.projectdose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estsoft/projectdose/ProjectDoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.projectdose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
