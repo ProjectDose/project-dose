f35fd18c1092585b7aff0e2b091314e70a98d0c7
jungyouknow <jyh7343@suwon.ac.kr>
2024-12-09 13:49:51 +0900

    setter 메서드 추가

--- src/main/java/com/estsoft/projectdose/users/entity/Users.java
+++ src/main/java/com/estsoft/projectdose/users/entity/Users.java
@@ -11,14 +11,16 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="user_id",nullable = false,unique = true)
+	@Column(name="user_id", nullable = false, unique = true)
 	private Long id;
 
 	@Column(name = "email", nullable = false, unique = true, length = 100)
@@ -41,5 +43,34 @@ public class Users {
 	private boolean isDeleted;
 
 	@Enumerated(EnumType.STRING)
-	private Role role = Role.ROLE_USER;
+	private Role role;
+
+	// Setter 메서드
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public void setJoindate(LocalDate joindate) {
+		this.joindate = joindate;
+	}
+
+	public void setIsDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 }
